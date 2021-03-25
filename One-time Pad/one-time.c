c02d915791890d1b9a013b724b3942fe23208f18
Apostolos Mavrogiannakis <apomaurogiannakis@hotmail.com>
2021-03-25 22:39:03 +0200

    First push of onetime

--- One-time Pad/one-time.c
+++ One-time Pad/one-time.c
@@ -1,6 +1,160 @@
 #include "one-time_lib.h"
 
-int main() {
+/* 
+	One-time Pad:
+		The One-Time-Pad (OTP) algorithm is a very simple but yet very strong algorithm in the sense
+		that it can not be cracked even with quantum computers. The algorithm pairs each plaintext with
+		a random secret key (also referred to as a one-time pad). Specifically, each bit or byte/character
+		of the plaintext is combined with the corresponding bit or byte/character from the random secret
+		key. One-time pad requires that the secret key is of the same size or longer than the plaintext.
+*/
+
+int 
+main(int argc, char* argv[]) {
+	char *keyword, *input_text;
+
+	plaintext = fopen(argv[1], "r");
+
+
+	if(plaintext == NULL) {
+		perror("Error opening file.");
+	}
+
+	int i, file_size = 0;
+	
+	fseek(plaintext, 0, SEEK_END);
+	input_size = ftell(plaintext);
+	fseek(plaintext, 0, SEEK_SET);
+
+	input_text = (char *) malloc(input_size);
+
+	fread(input_text, 1, input_size, plaintext);
+
+	keyword = "";
+
+	keyword = keyword_generator(input_size);
+
+	switch(__OPER__(argv[2])) {
+		case ENCRYPT:
+			output = fopen("encrypted.txt", "w");
+			if(output == NULL) {
+				perror("Error opening file.");
+				exit(0);
+			}
+
+			printf("Performing encryption\n");
+			ctxt = otp_encrypt(input_text, keyword);
+			
+			break;
+		case DECRYPT:
+			output = fopen("decrypted.txt", "w");
+			if(output == NULL) {
+				perror("Error opening file.");
+				exit(0);
+			}
+			printf("Performing decryption\n");
+			ctxt = otp_decrypt(input_text, keyword);
+
+			break;
+		default:
+			perror("Arrrrgh! No man's land has been reached.\n");
+			assert(0); /* No man's land. */
+			exit(0);
+	}
+
+
+	fprintf(output, "%s", ctxt);
+
+	fclose(plaintext);
+	fclose(output);
 
 	return 0;
 }
+
+char *
+otp_encrypt(char *input_text, char *keyword) {
+	int i = 0;
+	char *s;
+	char encrypted_c;
+
+	while(i < input_size)
+	{
+		if (__VALID__(input_text[i]))
+		{
+			printf("%s <- %s ^ %s\n", encrypted_c, keyword[i], input_text[i]);
+			encrypted_c = keyword[i] ^ input_text[i];
+			s = append_string(s, encrypted_c);
+
+		} else if (!__ES_CH__(input_text[i])){
+			/* If char is an escape character or a space, just ignore it.*/
+			fprintf(stderr,"UNRECOGNISED CHARACTER %c: File stream contains unrecognised character.\nThis algorithm supports Numbers[0-9] and Letters[A-Za-z].\n", input_text[i]);
+			exit(0);
+		}
+
+		i++;
+	}
+
+	return s;
+}
+
+char *
+otp_decrypt(char *input_text, char *keyword) {
+	int i = 0;
+	char *s;
+	char encrypted_c;
+
+	s = "";
+	while(i < input_size)
+	{
+		if (__VALID__(input_text[i]))
+		{
+			encrypted_c = keyword[i] ^ input_text[i];
+			s = append_string(s, encrypted_c);
+
+		} else if (!__ES_CH__(input_text[i])){
+			/* If char is an escape character or a space, just ignore it.*/
+			fprintf(stderr,"UNRECOGNISED CHARACTER %c: File stream contains unrecognised character.\nThis algorithm supports Numbers[0-9] and Letters[A-Za-z].\n", input_text[i]);
+			exit(0);
+		}
+
+		i++;
+	}
+
+	return s;
+}
+
+char *
+keyword_generator(int size)
+{
+	int i = 0;
+	char *s;
+	FILE* urand = fopen("/dev/urandom","r");
+
+	s = "";
+	while(i < size) {
+		char rand_char;
+		rand_char = fgetc(urand);
+
+		s = append_string(s, rand_char);
+		i++;
+	}
+
+	return s;
+}
+
+
+char * 
+append_string(char *string, char char_to_append){
+	char *temp;
+
+	temp = (char *) malloc(sizeof(char) * (strlen(string) + 1));
+
+	strcat(temp, string);
+
+	string = "";
+
+	strcat(temp, &char_to_append);
+
+	return temp;
+}
+
--- One-time Pad/one-time_lib.h
+++ One-time Pad/one-time_lib.h
@@ -1,7 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
+#include <assert.h>
 
-uint8_t* caesar_encrypt(uint8_t *plaintext, ushort N);
+#define TRUE				1
+#define FALSE				0
+#define ALP_SIZE 			62
+#define ASCII_NUM_START		48
+#define ASCII_NUM_END		57
+#define ASCII_BL_START		65
+#define ASCII_BL_END		90 
+#define ASCII_SL_START		97 
+#define ASCII_SL_END		122
+#define	__VALID__(c)		((c >= ASCII_SL_START && c <= ASCII_SL_END) || (c >= ASCII_NUM_START && c <= ASCII_NUM_END) || (c >= ASCII_BL_START && c <= ASCII_BL_END) ? 1 : 0)
+#define ENCRYPT 			201
+#define DECRYPT 			202
+#define __OPER__(c) ((strcmp(c, "encryption") == 0 || strcmp(c, "ENCRYPTION") == 0) ? ENCRYPT : ((strcmp(c, "decryption") == 0 || strcmp(c, "DECRYPTION") == 0) ? DECRYPT : FALSE))
+#define __ES_CH__(c) (((c == '\n') || (c == '\a') || (c == '\b') || (c == '\v') || (c == '\r') || (c == '\t') || (c == '\'') || (c == '\?') || (c == '\\') || (c == '\"') || (c == '\f') || (c == ' ') || (c == EOF)) ? TRUE : FALSE)
 
-uint8_t* caesar_decrypt(uint8_t *ciphertext, ushort N);
+long input_size;
+char *ctxt;
+
+FILE* plaintext;
+FILE* output;
+
+void __init__();
+
+char * append_string(char *string, char char_to_append);
+
+char * keyword_generator(int size);
+
+char * otp_encrypt(char *input_text, char *keyword);
+
+char * otp_decrypt(char *input_text, char *keyword);
