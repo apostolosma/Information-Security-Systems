6f99135e1e9ea34c076fe2d0828a487d1856c58d
Apostolos Mavrogiannakis <apomaurogiannakis@hotmail.com>
2021-04-03 20:00:18 +0300

    uploading playfair cipher

--- Playfair Cipher/playfair.c
+++ Playfair Cipher/playfair.c
@@ -1,6 +1,124 @@
 #include "playfair_lib.h"
 
-int main() {
+
+
+/*
+The Playfair cipher encrypts pairs of letters (digraphs), instead of single letters as is the case
+with simpler substitution ciphers such as the Caesar Cipher. The key element of this cipher is a
+5x5 grid that represents the key. In order to create the table, the letters of the key are placed in
+the grid, from left to right beginning from the first row. Then the rest of the alphabet’s letters are
+inserted in the grid alphabetically. Each letter is placed once in the grid. Since the 5x5 grid can
+only hold 25 characters J is usually omitted and treated as ‘I’. For example the key grid for the
+pass phrase “HELLO WORLD” will create the following key grid
+1
+
+*/
+
+int main(int argc, char* argv[]) {
+	FILE *f_plaintext = fopen(argv[1], "r");
+	FILE *output;
+
+	if(f_plaintext == NULL) {
+		perror("Error opening file.");
+	}
+
+	unsigned char *plaintext = read_input(f_plaintext);
+	
+	__init__(seen);
+
+	construct_grid(plaintext);
+
+	
+
+	/*
+	switch(__OPER__(argv[2])) {
+		case ENCRYPT:
+			output = fopen("encrypted.txt", "w");
+			if(output == NULL) {
+				perror("Error opening file.");
+				exit(0);
+			}
+			printf("Performing encryption\n");
+			perform_encryption();
+
+			break;
+		case DECRYPT:
+			output = fopen("decrypted.txt", "w");
+			if(output == NULL) {
+				perror("Error opening file.");
+				exit(0);
+			}
+			printf("Performing decryption\n");
+			perform_decryption();
+
+			break;
+		default:
+			perror("Arrrrgh! No man's land has been reached.\n");
+			assert(0); // No man's land.
+	}*/
 
 	return 0;
+}
+
+void
+construct_grid(unsigned char *plaintext) {
+	char c,encrypted_c;
+	int counter = 0, key;
+	int row = 0, col = 0;
+
+	while(c = *plaintext++) {
+		if (__VALID__(c))
+		{
+			key = find_key(c);
+			/* This means this is the first time we read this character. */
+			/* So we have to import it into our grid. */
+
+			if( !seen[key] ) {
+				if(wrap(row,col)) {
+					grid[row][col] = c;
+
+					row++; col = 0;
+				}
+				else {
+					grid[row][col] = c;
+					
+					col++;
+				}
+				seen[key] = 1; // Updating character in seen array.
+			}
+		} else if (!__ES_CH__(c)){
+			/* If char is an escape character or a space, just ignore it.*/
+			fprintf(stderr,"UNRECOGNISED CHARACTER %c: File stream contains unrecognised character.\nThis algorithm supports Numbers[0-9] and Letters[A-Za-z].\n", c);
+			exit(0);
+		}
+	}
+}
+
+int find_key(char c) {
+    return (int) c - (int) 'A';
+}
+
+bool wrap(int row,int col) {
+    if(col == 4) {
+        return true;
+    }
+        return false;
+}
+
+unsigned char *
+read_input(FILE *f_plaintext) {
+	unsigned int size;
+
+	fseek(f_plaintext,0L,SEEK_END);
+	size = ftell(f_plaintext);
+	rewind(f_plaintext);
+	
+	unsigned char *dummy = (char *)malloc(sizeof(char) * size);
+	unsigned char *plaintext = (char *)malloc(sizeof(char) * size);
+
+	while(fgets(dummy, size, f_plaintext)) {
+		strcat(plaintext, dummy);
+	}
+
+	return plaintext;
 }
--- Playfair Cipher/playfair_lib.h
+++ Playfair Cipher/playfair_lib.h
@@ -1,7 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
+#include <assert.h>
+#include <stdbool.h>
 
-uint8_t* caesar_encrypt(uint8_t *plaintext, ushort N);
+#define	__VALID__(c)		( (c >= 'A' && c <= 'Z') ? 1 : 0)
+#define __init__(c) ({\
+            for(int i = 0; i < 26; i++) { c[i] = 0; } \
+        })
 
-uint8_t* caesar_decrypt(uint8_t *ciphertext, ushort N);
+#define __ES_CH__(c) (((c == '\n') || (c == '\a') || (c == '\b') || (c == '\v') || (c == '\r') || (c == '\t') || (c == '\'') || (c == '\?') || (c == '\\') || (c == '\"') || (c == '\f') || (c == ' ') || (c == EOF)) ? true : false)
+
+
+unsigned char grid[5][5];
+
+unsigned int seen[26];
+
+unsigned char* playfair_encrypt(unsigned char *plaintext, unsigned char** key);
+
+unsigned char* playfair_decrypt(unsigned char *ciphertext, unsigned char** key);
+
+void construct_grid(unsigned char *plaintext);
+
+int find_key(char c);
+
+bool wrap(int row, int col);
+
+unsigned char *read_input(FILE *f_plaintext);
